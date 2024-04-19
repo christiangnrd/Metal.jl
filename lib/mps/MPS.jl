00ba5d9cff1cfcbcde38fda19449b35680c1ef68
Christian Guinard <28689358+christiangnrd@users.noreply.github.com>
2024-04-19 11:51:39 -0300

    Move filters to new file

--- lib/mps/MPS.jl
+++ lib/mps/MPS.jl
@@ -26,6 +26,9 @@ include("size.jl")
 include("command_buf.jl")
 include("kernel.jl")
 include("images.jl")
+include("imagefilters.jl")
+
+# high-level wrappers
 include("matrix.jl")
 include("vector.jl")
 include("matrixrandom.jl")
