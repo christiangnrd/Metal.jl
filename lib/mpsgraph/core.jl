35e04d85dd6e23c7ccc4e109509e1733bb0a7672
Christian Guinard <28689358+christiangnrd@users.noreply.github.com>
2024-09-05 22:38:29 -0300

    Initial MPSGraph Stuff

---
+++ lib/mpsgraph/core.jl
@@ -0,0 +1,32 @@
+# Contains definitions for api from MPSGraphCore.h, MPSGraphDevice.h
+
+## MPSGraphCore.h
+@objcwrapper MPSGraphObject <: NSObject
+
+@objcwrapper Graph <: MPSGraphObject
+
+@objcwrapper MPSGraphType <: MPSGraphObject
+
+@objcwrapper MPSGraphShapedType <: MPSGraphType
+
+@objcproperties MPSGraphShapedType begin
+    # @autoproperty shape::id{MPSShape} # Not doable yet as MPSShape is NSArray of NSNumber
+    @autoproperty dataType::MPSDataType setter=setDataType
+end
+
+# function MPSGraphShapedType(shape::MPSShape)
+#     TODO
+# end
+
+## MPSGraphDevice.h
+@objcwrapper MPSGraphDevice <: MPSGraphType
+
+@objcproperties MPSGraphDevice begin
+    @autoproperty type::MPSGraphDeviceType setter=setDataType
+    @autoproperty metalDevice::id{MTLDevice} # Not doable yet as MPSShape is NSArray of NSNumber
+end
+
+function MPSGraphDevice(device::MTLDevice)
+    dev = @objc [MPSGraphDevice deviceWithMTLDevice:metalDevice::id{MTLDevice}]::id{MPSGraphDevice}
+    MPSGraphDevice(dev)
+end
