b2f34480c624d02be00b78d96e02423a3e23311e
Christian Guinard <28689358+christiangnrd@users.noreply.github.com>
2024-12-17 20:25:09 -0400

    Initial MPSGraph support

--- src/Metal.jl
+++ src/Metal.jl
@@ -51,6 +51,8 @@ include("compiler/reflection.jl")
 # libraries
 include("../lib/mps/MPS.jl")
 export MPS
+include("../lib/mpsgraphs/MPSGraphs.jl")
+export MPSGraphs
 
 # array implementation
 include("utilities.jl")
