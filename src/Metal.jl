6abaa1c6a13a3cd493a478301d73e1ef5f50efea
Christian Guinard <28689358+christiangnrd@users.noreply.github.com>
2025-01-28 22:06:48 -0400

    Use MPSGraph for matrix multiplication

--- src/Metal.jl
+++ src/Metal.jl
@@ -54,6 +54,9 @@ export MPS
 include("../lib/mpsgraphs/MPSGraphs.jl")
 export MPSGraphs
 
+# LinearAlgebra
+include("linalg.jl")
+
 # array implementation
 include("utilities.jl")
 include("broadcast.jl")
